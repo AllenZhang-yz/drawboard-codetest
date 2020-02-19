27e0c0b210b3a62b38586ddaf1f0fa211b0bf595
Allen Zhang <allen.zhang018@gmail.com>
2020-02-19 13:07:44 +1100

    change http to https

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const fetchMatchedStations = inputValue => {
   return axios.get(
-    `http://api.waqi.info/search/?keyword=${inputValue}&token=b04f71f01d3fa37585097b6e8b99bfbdb90de1aa`
+    `https://api.waqi.info/search/?keyword=${inputValue}&token=b04f71f01d3fa37585097b6e8b99bfbdb90de1aa`
   );
 };
 
 export const fetchAQI = inputValue => {
   return axios.get(
-    `http://api.waqi.info/feed/${inputValue}/?token=b04f71f01d3fa37585097b6e8b99bfbdb90de1aa`
+    `https://api.waqi.info/feed/${inputValue}/?token=b04f71f01d3fa37585097b6e8b99bfbdb90de1aa`
   );
 };
